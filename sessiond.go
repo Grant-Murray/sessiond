ef4313983ea2794321c4bd80ba7164bb0233fbf2
Grant Murray <email@grantmurray.com>
2013-11-28 09:28:33 -0800

    Removed logging

--- sessiond.go
+++ sessiond.go
@@ -1,44 +1,33 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
-	"github.com/Grant-Murray/logdb"
-	"github.com/Grant-Murray/session"
-	"net"
-	"net/http"
+  "crypto/tls"
+  "fmt"
+  "github.com/Grant-Murray/session"
+  "net"
+  "net/http"
 )
 
 func main() {
-	session.Configure()
-	// session.log is created in plog-pg-schema.sql
-	err := logdb.Initialize(session.Conf.DatabaseHandle, "", "INSERT INTO session.log (entered, msg, level) VALUES (now(), $1, $2)")
-	if err != nil {
-		panic(fmt.Sprintf("Failed to initialize logdb: %s", err))
-	}
-	logdb.Info.Print("plogd server started")
-	if !session.Conf.DebugVerbosely {
-		logdb.Debug.Print(logdb.StopWriting)
-	}
 
-	var tlsc tls.Config
-	tlsc.NextProtos = []string{"http/1.1"}
-	tlsc.Certificates = make([]tls.Certificate, 1)
-	if tlsc.Certificates[0], err = tls.X509KeyPair(session.Conf.HttpsCert, session.Conf.HttpsKey); err != nil {
-		logdb.Attn.Fatalf("Error attempting to use HttpsCert and HttpsKey: %s", err.Error())
-	} else {
-		logdb.Debug.Printf("tls certificate was created")
-	}
+  var err error
 
-	http.HandleFunc("/session/", session.Handler)
+  var tlsc tls.Config
+  tlsc.NextProtos = []string{"http/1.1"}
+  tlsc.Certificates = make([]tls.Certificate, 1)
+  if tlsc.Certificates[0], err = tls.X509KeyPair(session.Conf.HttpsCert, session.Conf.HttpsKey); err != nil {
+    panic("Error attempting to use HttpsCert and HttpsKey to create a certificate: " + err.Error())
+  }
 
-	addr := fmt.Sprintf("%s:%d", session.Conf.HttpsHost, session.Conf.HttpsPort)
-	tlsServer := &http.Server{Addr: addr, TLSConfig: &tlsc}
-	conn, err := net.Listen("tcp", addr)
-	if err != nil {
-		logdb.Attn.Fatalf("Error while listening: %s", err.Error())
-	}
+  http.HandleFunc("/session/", session.Handler)
 
-	tlsListener := tls.NewListener(conn, &tlsc)
-	tlsServer.Serve(tlsListener)
+  addr := fmt.Sprintf("%s:%d", session.Conf.HttpsHost, session.Conf.HttpsPort)
+  tlsServer := &http.Server{Addr: addr, TLSConfig: &tlsc}
+  conn, err := net.Listen("tcp", addr)
+  if err != nil {
+    panic("Error while listening: " + err.Error())
+  }
+
+  tlsListener := tls.NewListener(conn, &tlsc)
+  tlsServer.Serve(tlsListener)
 }
